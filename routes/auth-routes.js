01b01a019bef9f4849422e1b5c7119c50c0ed0e3
Adam Simonini <adamsimonini@users.noreply.github.com>
2021-03-13 13:06:14 -0500

    fix linting problem

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,7 +3,7 @@ const controller = require("../controllers/auth-controller");
 
 module.exports = function(app) {
   app.use((req, res, next) => {
-      res.header(
+    res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
     );
